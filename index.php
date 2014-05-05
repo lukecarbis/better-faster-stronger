bfaa2bfa53c62ed5364a197304c12cd3b366341a
Luke Carbis <luke.carbis@x-team.com>
2014-05-06 08:27:54 +1000

    Initial slideshow setup

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Better Faster Stronger</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<link href="assets/style.css" rel="stylesheet" type="text/css" />
+		<style type="text/css"></style>
+	</head>
+	<body>
+		<textarea id="source"><?php include( 'presentation.md' ); ?></textarea>
+		<script src="http://gnab.github.io/remark/downloads/remark-latest.min.js" type="text/javascript">
+		</script>
+		<script type="text/javascript">
+			var slideshow = remark.create();
+		</script>
+	</body>
+</html>
---
+++ presentation.md
@@ -0,0 +1,18 @@
+name: title
+class: center, middle, inverse
+
+# .green[Better] .red[Faster] .blue[Stronger]
+### Best Practices for Coding in WordPress
+
+[View this presentation on [GitHub](https://github.com/lukecarbis/better-faster-stronger)]
+
+---
+
+# Agenda
+
+1. Coding Standards
+2. Scripts & Styles
+3. AJAX
+4. Namespacing
+5. Database Queries
+
